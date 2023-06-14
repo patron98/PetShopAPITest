c5669297dd7a56dcbd30ecb02cbd0f8ac3fcd9ae
Jim Schyvens <jim.schyvens@bignited.be>
2023-06-14 18:15:52 +0200

    changed small mistake in put request

--- Keywords/com/patron/AddAndCheckNewPet.groovy
+++ Keywords/com/patron/AddAndCheckNewPet.groovy
@@ -15,10 +15,10 @@ import internal.GlobalVariable as GlobalVariable
 
 public class AddAndCheckNewPet {
 
-	
+
 	ResponseObject response
 
-	def addNewDog = { 
+	def addNewDog = {
 
 		response = WS.sendRequest(findTestObject('POST New Pet'))
 		WS.verifyResponseStatusCode(response, 200)
@@ -32,53 +32,53 @@ public class AddAndCheckNewPet {
 		String getRequestUrl = getRequest.getRestUrl().replace('{id}', id)
 		getRequest.setRestUrl(getRequestUrl)
 		response = WS.sendRequest(getRequest)
-		
+
 		logInfo("Dog Name: " + (String) WS.getElementPropertyValue(response, 'category.name'))
 		logInfo("Dog Status: " + (String) WS.getElementPropertyValue(response, 'status'))
-		
+
 		WS.verifyResponseStatusCode(response, 200)
 		WS.verifyElementPropertyValue(response, 'category.name', GlobalVariable.DogName)
 	}
-	
+
 	def updateDogByID = { String id, String status ->
 		RequestObject putRequest = findTestObject('PUT Update Pet')
 		String putRequestUrl = putRequest.getRestUrl().replace('{id}', id)
 		String requestBody = """
-{
-  "id": 0,
-  "category": {
-    "id": 0,
-    "name": "${GlobalVariable.DogName}"
-  },
-  "name": "${GlobalVariable.DogName}",
-  "photoUrls": [
-    ""
-  ],
-  "tags": [
-    {
-      "id": 0,
-      "name": "stafford"
-    }
-  ],
-  "status": "${status}"
-}
-"""
+			{
+				"id": 0,
+				"category": {
+					"id": 0,
+					"name": "${GlobalVariable.DogName}"
+					},
+				"name": "${GlobalVariable.DogName}",
+				"photoUrls": [
+					""
+				],
+				"tags": [
+					{
+						"id": 0,
+						"name": "stafford"
+					}
+				],
+				"status": "${status}"
+			}
+		"""
 		putRequest.setHttpBody(requestBody)
 		response = WS.sendRequest(putRequest)
-		
+
 		logInfo("Dog Name: " + (String) WS.getElementPropertyValue(response, 'category.name'))
 		logInfo("Status: " + (String) WS.getElementPropertyValue(response, 'status'))
-		
+
 		WS.verifyResponseStatusCode(response, 200)
 		WS.verifyElementPropertyValue(response, "status", status)
 	}
-	
-	def deleteDogByID = { String id -> 
+
+	def deleteDogByID = { String id ->
 		RequestObject deleteRequest = findTestObject('DELETE Pet by ID')
 		String deleteRequestUrl = deleteRequest.getRestUrl().replace('{id}', id)
 		deleteRequest.setRestUrl(deleteRequestUrl)
 		response = WS.sendRequest(deleteRequest)
-		
+
 		WS.verifyResponseStatusCode(response, 200)
 	}
 }
